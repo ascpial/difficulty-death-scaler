9fc70e02d9b3fd7078c12ce8cae6cc47d5235229
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-12-26 14:49:43 +0000

    feat(global): improved ai mobs can have higher safe fall distance

---
+++ src/main/java/world/anhgelus/architectsland/difficultydeathscaler/mixin/AnnoyingMobEntity.java
@@ -0,0 +1,35 @@
+package world.anhgelus.architectsland.difficultydeathscaler.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import world.anhgelus.architectsland.difficultydeathscaler.difficulty.global.GlobalDifficultyManager;
+
+@Mixin(MobEntity.class)
+public abstract class AnnoyingMobEntity extends LivingEntity {
+    protected AnnoyingMobEntity(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getSafeFallDistance", cancellable = true)
+    public void newSafeFallDistance(CallbackInfoReturnable<Integer> cir) {
+        final var t = (MobEntity) (Object) this;
+        if (t instanceof SkeletonEntity && GlobalDifficultyManager.areSkeletonsBetter())
+            cir.setReturnValue(safeFallDistance(t));
+        else if (t instanceof ZombieEntity && GlobalDifficultyManager.areZombiesBetter())
+            cir.setReturnValue(safeFallDistance(t));
+    }
+
+    @Unique
+    private int safeFallDistance(MobEntity target) {
+        return target == null ? getSafeFallDistance(0.0F) : getSafeFallDistance(this.getHealth() - 3.0F);
+    }
+}
